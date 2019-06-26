33915bf2c4f319c486e3475d7f4e3ce5ae3bbff7
Rainer Kartmann <rainer.kartmann@student.kit.edu>
2019-06-26 11:45:46 +0200

    Add exceptions

---
+++ VirtualRobot/XML/mujoco/exceptions.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <VirtualRobot/VirtualRobotException.h>
+
+
+namespace VirtualRobot::mujoco::error
+{
+
+    /// Indicates that no body exists or has been added for a robot node.
+    class NoBodyOfRobotNode : public VirtualRobotException
+    {
+    public:
+        NoBodyOfRobotNode(const std::string& nodeName);
+    private:
+        static std::string makeMsg(const std::string& nodeName);
+    };
+    
+    
+    /// Indicates that a robot node has to be a joint node, but is not.
+    class NodeIsNoJoint : public VirtualRobotException
+    {
+    public:
+        NodeIsNoJoint(const std::string& nodeName);
+    };
+
+
+}
