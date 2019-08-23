13b9086b105d6d94cb58b459e9bf9000f6ffa23a
Rainer Kartmann <rainer.kartmann@kit.edu>
2019-08-23 10:42:54 +0200

    Add more useful exception messages in read_json/write_json

--- VirtualRobot/Util/json/io.cpp
+++ VirtualRobot/Util/json/io.cpp
@@ -1,33 +1,65 @@
 #include "io.h"
 
 #include <iostream>
+#include <filesystem>
 #include <fstream>
+
+namespace fs = std::filesystem;
 
 
 namespace nlohmann
 {
+
+    static void checkExists(const std::string& filename, const std::string& prefix = "")
+    {
+        if (!fs::exists(filename))
+        {
+            throw std::ios_base::failure(prefix + "File \"" + filename + "\" does not exist.");
+        }
+    }
+
 
     json read_json(const std::string& filename)
     {
         std::ifstream ifs;
         // Allow throwing std::ios_base::failure.
         ifs.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-        
-        ifs.open(filename);
-        json j;
-        ifs >> j;
-        return j;
+
+        try
+        {
+            ifs.open(filename);
+            json j;
+            ifs >> j;
+            return j;
+        }
+        catch (const std::ios_base::failure& e)
+        {
+            // Add a more useful message.
+            const std::string msg = "Failed to read file \"" + filename + "\": ";
+            checkExists(filename, msg);
+            throw std::ios_base::failure(msg + std::string(e.what()));
+        }
     }
-    
+
+
     void write_json(const std::string& filename, const json& j,
                     const int indent, const char indent_char)
     {
         std::ofstream ofs;
         // Allow throwing std::ios_base::failure.
         ofs.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-        
-        ofs.open(filename);
-        ofs << j.dump(indent, indent_char);
+
+        try
+        {
+            ofs.open(filename);
+            ofs << j.dump(indent, indent_char);
+        }
+        catch (const std::ios_base::failure& e)
+        {
+            // Add a more useful message.
+            const std::string msg = "Failed to write file \"" + filename + "\": ";
+            throw std::ios_base::failure(msg + std::string(e.what()));
+        }
     }
 
 }
