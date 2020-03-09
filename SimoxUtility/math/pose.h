9e1870a9b8d4b43341a8908b30d9329e99d1f667
Rainer Kartmann <rainer.kartmann@kit.edu>
2020-03-09 09:43:56 +0100

    Add check_rotation_matrix (throws simox::error::InvalidRotationMatrix)

--- SimoxUtility/math/pose.h
+++ SimoxUtility/math/pose.h
@@ -2,6 +2,7 @@
 
 // This file is generated!
 
+#include "pose/check_rotation_matrix.h"
 #include "pose/invert.h"
 #include "pose/orthogonalize.h"
 #include "pose/pose.h"
---
+++ SimoxUtility/math/pose/check_rotation_matrix.cpp
@@ -0,0 +1,45 @@
+#include "check_rotation_matrix.h"
+
+#include <sstream>
+
+#include <Eigen/Dense>  // for .determinant()
+
+#include "orthogonalize.h"
+
+
+void simox::math::check_rotation_matrix(const Eigen::Matrix3f& rotation, float precision)
+{
+    // ARMARX_CHECK(simox::math::is_matrix_orthogonal(rotation)) << VAROUT(rotation);
+    // ARMARX_CHECK_CLOSE(rotation.determinant(), 1.f, 1e-6f) << VAROUT(rotation);
+
+    if (!simox::math::is_matrix_orthogonal(rotation, precision))
+    {
+        throw simox::error::InvalidRotationMatrix(rotation, "Rotation matrix is not orthogonal.");
+    }
+    if (std::abs(rotation.determinant() - 1.f) > precision)
+    {
+        throw simox::error::InvalidRotationMatrix(rotation, "Determinant of rotation matrix is not 1.0.");
+    }
+}
+
+
+namespace simox::error
+{
+
+    InvalidRotationMatrix::InvalidRotationMatrix(const Eigen::Matrix3f matrix, const std::string& message) :
+        SimoxError(make_msg(matrix, message))
+    {}
+
+    std::string InvalidRotationMatrix::make_msg(const Eigen::Matrix3f matrix, const std::string& message)
+    {
+        std::stringstream ss;
+        if (!message.empty())
+        {
+            ss << message << "\n";
+        }
+        ss << "Matrix R: \n" << matrix << "\n";
+        ss << "Determinant: " << matrix.determinant() << "\n";
+        ss << "Matrix R * R^T\n" << matrix * matrix.transpose() << "\n";
+        return ss.str();
+    }
+}
