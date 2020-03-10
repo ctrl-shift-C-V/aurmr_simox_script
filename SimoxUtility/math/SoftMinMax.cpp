cf372668f68b2a64e0c15f539edc3dbf1f2dcbce
Rainer Kartmann <rainer.kartmann@kit.edu>
2020-03-10 13:50:16 +0100

    Add SoftMinMax (from SemanticObjectRelations)

---
+++ SimoxUtility/math/SoftMinMax.cpp
@@ -0,0 +1,107 @@
+#include "SoftMinMax.h"
+
+#include <cmath>
+#include <stdexcept>
+#include <sstream>
+
+namespace simox::math
+{
+
+    SoftMinMax::SoftMinMax()
+    {
+        reset(0, 1);
+    }
+
+    SoftMinMax::SoftMinMax(float percentile, std::size_t numValues)
+    {
+        reset(percentile, numValues);
+    }
+
+    void SoftMinMax::reset(float percentile, std::size_t numValues)
+    {
+        min_queue = MinQueue();
+        max_queue = MaxQueue();
+
+        if (percentile < 0 || percentile > 0.5f)
+        {
+            std::stringstream msg;
+            msg << "percentile must be in [0, 0.5], but was " << percentile;
+            throw std::invalid_argument(msg.str());
+        }
+        if (numValues == 0)
+        {
+            std::stringstream msg;
+            msg << "numValues must be > 0, but was " << numValues;
+            throw std::invalid_argument(msg.str());
+        }
+        this->percentile = percentile;
+        this->num_elements = numValues;
+    }
+
+    void SoftMinMax::add(float value)
+    {
+        if (min_queue.size() != max_queue.size())
+        {
+            std::stringstream msg;
+            msg << "Invariant violated: minQueue.size() == maxQueue.size(), "
+                << "but were " << min_queue.size() << " and " << max_queue.size();
+            throw std::logic_error(msg.str());
+        }
+
+        if (min_queue.size() < allowed_heap_size())
+        {
+            // heaps not full yet
+            min_queue.push(value);
+            max_queue.push(value);
+            return;
+        }
+
+        // heaps are full
+
+        // check minQueue
+        if (value < min_queue.top())
+        {
+            // insert and pop
+            min_queue.push(value);
+            min_queue.pop();
+        } // else ignore value
+
+        // check maxQueue
+        if (value > max_queue.top())
+        {
+            max_queue.push(value);
+            max_queue.pop();
+        }
+
+    }
+
+    float SoftMinMax::get_soft_min() const
+    {
+        if (max_queue.empty())
+        {
+            throw std::out_of_range("Calling getSoftMin() but no element was added.");
+        }
+        return min_queue.top();
+    }
+
+    float SoftMinMax::get_soft_max() const
+    {
+        if (max_queue.empty())
+        {
+            throw std::out_of_range("Calling getSoftMin() but no element was added.");
+        }
+
+        return max_queue.top();
+    }
+
+    std::size_t SoftMinMax::num_outside_soft_min_max() const
+    {
+        return size_t(std::ceil(percentile * num_elements));
+    }
+
+    std::size_t SoftMinMax::allowed_heap_size() const
+    {
+        return std::max(std::size_t(1), num_outside_soft_min_max());
+    }
+
+}
---
+++ SimoxUtility/math/SoftMinMax.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <queue>
+
+
+namespace simox::math
+{
+
+    /**
+     * @brief The SoftMinMax class can be used to find soft min and max of a set of floats.
+     * Soft means that some values are allowed to be >= soft min / <= soft max
+     * (including the soft min/max), respectively.
+     * A percentile argument in [0..0.5] specifies the percentage of values
+     * which are allowed to excess the soft min/max.
+     */
+    class SoftMinMax
+    {
+    public:
+
+        /// No initialization constructor.
+        SoftMinMax();
+
+        /**
+         * @brief Constructs a SoftMinMax for given percentile and number of values.
+         *
+         * @param percentile
+         *  the percentage of values that may excess the soft min/max. Must be in [0..0.5].
+         * @param numValues the total number of values that will be added. Must be > 0.
+         *
+         * @throws `std::invalid_argument`
+         *  If one of the parameters value does not meet the requirements
+         */
+        SoftMinMax(float percentile, std::size_t numValues);
+
+        /**
+         * Reinitializes the SoftMinMax with the given arguments.
+         * @see SoftMinMax()
+         * @throws std::invalid_argument if one of the parameters value does not meet the requirements
+         */
+        void reset(float percentile, std::size_t numValues);
+
+        /// Add a value to the considered collection.
+        /// @note Only values excessing the current soft min/max are stored.
+        void add(float value);
+
+        /// Get the current soft min.
+        /// @throws `std::out_of_range` if no element was added
+        float get_soft_min() const;
+        /// Get the current soft max.
+        /// @throws `std::out_of_range` if no element was added
+        float get_soft_max() const;
+
+
+    private:
+
+        /// Returns the number of elements that are allowed to be <= softMin / >= softMax.
+        std::size_t num_outside_soft_min_max() const;
+
+        /// The allowed size of the priority queues
+        std::size_t allowed_heap_size() const;
+
+
+        /// The percentile in [0, 0.5]
+        float percentile = 0;
+        /// The number of elements to be added.
+        std::size_t num_elements = 0;
+
+
+        using Container = std::vector<float>;
+        using MaxCompare = std::less<float>;     // Results in max heap.
+        using MinCompare = std::greater<float>;  // Results in min heap.
+
+        using MinQueue = std::priority_queue<float, Container, MaxCompare>;
+        using MaxQueue = std::priority_queue<float, Container, MinCompare>;
+
+        /// Stores all elements <= softMin == top()
+        MinQueue min_queue;
+        /// Stores all elements >= softMax == top()
+        MaxQueue max_queue;
+        // Invariant: minQueue.size() == maxQueue.size()
+
+    };
+
+}
