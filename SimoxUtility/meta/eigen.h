b7aafe3da082c47d0413d4da82ccb076c1de20e9
Christian R. G. Dreher <c.dreher@kit.edu>
2020-04-04 09:26:08 +0200

    Add is_eigen_matrix type

--- SimoxUtility/meta/eigen.h
+++ SimoxUtility/meta/eigen.h
@@ -3,3 +3,4 @@
 // This file is generated!
 
 #include "eigen/enable_if_compile_time_size.h"
+#include "eigen/is_eigen_matrix.h"
---
+++ SimoxUtility/meta/eigen/is_eigen_matrix.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <type_traits>
+
+#include <Eigen/Core>
+
+namespace simox::meta
+{
+
+    template<class T>
+    struct is_eigen_matrix : std::false_type {};
+
+
+    template <class ScalarType, int Rows, int Cols>
+    struct is_eigen_matrix<Eigen::Matrix<ScalarType, Rows, Cols>> : std::true_type
+    {
+        static constexpr int rows = Rows;
+        static constexpr int cols = Cols;
+        using scalar_t = ScalarType;
+        using eigen_t = Eigen::Matrix<ScalarType, Rows, Cols>;
+    };
+
+
+    template<class T>
+    static constexpr bool is_eigen_matrix_v = is_eigen_matrix<T>::value;
+
+}
