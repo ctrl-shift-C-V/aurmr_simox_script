9c6aa5de10108631346926b56e88958e67115e93
Rainer Kartmann <rainer.kartmann@kit.edu>
2020-05-18 13:25:34 +0200

    Fix usage of std::isfinite

--- SimoxUtility/math/is_finite.h
+++ SimoxUtility/math/is_finite.h
@@ -3,18 +3,20 @@
 #include <Eigen/Core>
 #include <cmath>
 
+
 namespace simox::math
 {
-    using std::is_finite;
+    // using std::isfinite;  // Is this really necessary?
 
-    template<class Derived>
+
+    template <class Derived>
     bool is_finite(const Eigen::MatrixBase<Derived>& mat)
     {
         for (int x = 0; x < mat.rows(); ++x)
         {
             for (int y = 0; y < mat.cols(); ++y)
             {
-                if (!is_finite(mat(x, y)))
+                if (!std::isfinite(mat(x, y)))
                 {
                     return false;
                 }
