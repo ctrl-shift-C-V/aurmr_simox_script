fed0256bf37387c4e4c19640f8fff3def803e405
Raphael Grimm <raphael.grimm@kit.edu>
2020-05-26 14:25:17 +0200

    Add is_floating_point_eigen_array_v

--- SimoxUtility/meta/eigen/is_eigen_array.h
+++ SimoxUtility/meta/eigen/is_eigen_array.h
@@ -24,8 +24,11 @@ namespace simox::meta
         using eigen_t = Eigen::Array<ScalarType, Rows, Cols>;
     };
 
-
     template<class T>
     static constexpr bool is_eigen_array_v = is_eigen_array<T>::value;
 
+    template<class T>
+    static constexpr bool is_floating_point_eigen_array_v =
+        is_eigen_array_v<T>&&
+        std::is_floating_point_v<typename is_eigen_array<T>::scalar_t>;
 }
