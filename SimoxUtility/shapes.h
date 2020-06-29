1595c60fec32641a9d90054ac0f25d0a2eec7d0d
Rainer Kartmann <rainer.kartmann@kit.edu>
2020-06-29 08:59:06 +0200

    Update generated header

--- SimoxUtility/shapes.h
+++ SimoxUtility/shapes.h
@@ -3,6 +3,9 @@
 // This file is generated!
 
 #include "shapes/AxisAlignedBoundingBox.h"
+#include "shapes/CircleBase.h"
 #include "shapes/OrientedBox.h"
 #include "shapes/OrientedBoxBase.h"
+#include "shapes/OrientedEllipsis.h"
+#include "shapes/OrientedEllipsisBase.h"
 #include "shapes/XYConstrainedOrientedBox.h"
