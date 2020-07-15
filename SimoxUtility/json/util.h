21ff83af340cf8601cb48b40a319b1ac31177317
Rainer Kartmann <rainer.kartmann@kit.edu>
2020-07-15 17:31:19 +0200

    Add JSON utility

---
+++ SimoxUtility/json/util.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <optional>
+
+#include "json.hpp"
+
+
+namespace simox::json
+{
+
+    /**
+     * @brief Get the value at the first key in `keys` contained in `j`.
+     * @param j The JSON object.
+     * @param keys The keys.
+     * @return The value at the first contained key.
+     *
+     * @throw std::out_of_range If none of the keys is found in `j`.
+     */
+    nlohmann::json::const_reference at_any_key(const nlohmann::json& j, const std::vector<std::string>& keys);
+
+    template <class T>
+    T get_at_any_key(const nlohmann::json& j, const std::vector<std::string>& keys)
+    {
+        return at_any_key(j, keys).get<T>();
+    }
+
+}
