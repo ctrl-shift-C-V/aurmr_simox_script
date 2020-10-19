43d7cdd876fa8444954b64a12279df1c3a4c24c8
Rainer Kartmann <rainer.kartmann@kit.edu>
2020-10-19 14:52:39 +0200

    Add SimoxPath

---
+++ SimoxUtility/simox/SimoxPath.h
@@ -0,0 +1,61 @@
+#include <filesystem>
+
+
+namespace simox
+{
+    class SimoxPath
+    {
+    public:
+
+        static std::filesystem::path getSimoxDir()
+        {
+            init();
+            return simoxRootPath;
+        }
+
+        static std::filesystem::path getSimoxUtilityDir()
+        {
+            return getSimoxDir() / "SimoxUtility";
+        }
+
+        static std::filesystem::path getVirtualRobotDir()
+        {
+            return getSimoxDir() / "VirtualRobot";
+        }
+        static std::filesystem::path getVirtualRobotDataDir()
+        {
+            return getVirtualRobotDir() / "data";
+        }
+
+        static std::filesystem::path getSimDynamicsDir()
+        {
+            return getSimoxDir() / "SimDynamics";
+        }
+
+        static std::filesystem::path getGraspPlanningDir()
+        {
+            return getSimoxDir() / "GraspPlanning";
+        }
+
+        static std::filesystem::path getMotionPlanningDir()
+        {
+            return getSimoxDir() / "MotionPlanning";
+        }
+
+
+    private:
+        SimoxPath();
+
+        static void init();
+        static bool checkPath(const char* path);
+        static bool checkPath(const std::filesystem::path& path);
+        static std::filesystem::path checkCandidatePaths(
+                const std::vector<std::string>& candidates, std::filesystem::path prefix = "");
+
+    private:
+
+        static bool simoxPathInitialized;
+        static std::filesystem::path simoxRootPath;
+
+    };
+}
