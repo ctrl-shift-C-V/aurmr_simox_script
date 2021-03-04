5c109106a521195a93a423199e72ee76165b4890
Fabian Reister <fabian.reister@kit.edu>
2021-03-04 10:26:36 +0100

    stupid me. content of test XYIndexRangeIterator was in NestedRangeIterator.cpp

--- SimoxUtility/tests/iterator/XYIndexRangeIterator.cpp
+++ SimoxUtility/tests/iterator/XYIndexRangeIterator.cpp
@@ -13,32 +13,27 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
- * @package    SimoxUtility/Iterators
+ * @package    
  * @author     Fabian Reister ( fabian dot reister at kit dot edu )
- * @date       2019
+ * @date       2021
  * @copyright  http://www.gnu.org/licenses/gpl-2.0.txt
  *             GNU General Public License
  */
 
-#include <algorithm>
-#include <numeric>
-#define BOOST_TEST_MODULE SimoxUtility/iterator/NestedRangeIterator
+#include <boost/test/unit_test_suite.hpp>
+#define BOOST_TEST_MODULE SimoxUtility/iterator/XYIndexRangeIterator
 
 #include <boost/test/included/unit_test.hpp>
 
 #include <vector>
 #include <iostream>
+#include <numeric>
+#include <algorithm>
 
 #include <SimoxUtility/iterator.h>
 
 
-// namespace NestedRangeIterator
-// {
-
-// }
-
-
-BOOST_AUTO_TEST_SUITE(NestedRangeIterator)
+BOOST_AUTO_TEST_SUITE(XYIndexRangeIterator)
 
 
 BOOST_AUTO_TEST_CASE(test_loop)
@@ -48,18 +43,55 @@ BOOST_AUTO_TEST_CASE(test_loop)
     constexpr int M = 20;
 
     // create 2D vector of size N*M
-    auto v = std::vector<std::vector<int>>(N, std::vector<int>(M, 1));
+    const auto v = std::vector<std::vector<int>>(N, std::vector<int>(M, 1));
 
-    auto it = simox::iterator::NestedRangeIterator(v);  
-    const int sum = std::accumulate(it.begin(), it.end(), 0);
+    const auto range_it = simox::iterator::XYIndexRangeIterator(v);
+    
+    // max counters for test checks
+    int x_max = 0;
+    int y_max = 0;
+    int cnt = 0;
 
-    // std::for_each(it.begin(), it.end(), [](auto i){});
+    for(const auto [x,y] : range_it){
+        x_max = std::max(x, x_max);
+        y_max = std::max(x, y_max);
 
-    //int sum = 200;
-    BOOST_CHECK_EQUAL(sum, N*M);
+        cnt++;
+    }
 
-    std::cout << "finished";
+    BOOST_CHECK_EQUAL(x_max, N-1);
+    BOOST_CHECK_EQUAL(y_max, M-1);
+    BOOST_CHECK_EQUAL(cnt, N*M);
+}
 
+
+BOOST_AUTO_TEST_CASE(test_foreach)
+{
+    // dimensions
+    constexpr int N = 10;
+    constexpr int M = 20;
+
+    // create 2D vector of size N*M
+    const auto v = std::vector<std::vector<int>>(N, std::vector<int>(M, 1));
+
+    const auto range_it = simox::iterator::XYIndexRangeIterator(v);
+    
+    int x_max = 0;
+    int y_max = 0;
+    int cnt = 0;
+
+    std::for_each(range_it.begin(), range_it.end(), [&](const auto& pnt){
+        const auto [x,y] = pnt;
+
+        x_max = std::max(x, x_max);
+        y_max = std::max(x, y_max);
+
+        cnt++;
+    });
+
+    BOOST_CHECK_EQUAL(x_max, N-1);
+    BOOST_CHECK_EQUAL(y_max, M-1);
+    BOOST_CHECK_EQUAL(cnt, N*M);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
