6000be4cb2946a73f82358d6785e0665fdfe5ea7
fabian.peller-konrad@kit.edu <fabian.peller-konrad@kit.edu>
2021-05-10 10:18:31 +0200

    added vector utility

---
+++ SimoxUtility/algorithm/vector.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <vector>
+
+
+namespace simox::alg
+{
+
+    template <typename TypeT>
+    std::vector<TypeT>
+    appended(const std::vector<TypeT>& v1, const std::vector<TypeT>& v2)
+    {
+        std::vector<TypeT> ret;
+        for (const auto& el : v1)
+        {
+            ret.emplace_back(el);
+        }
+        for (const auto& el : v2)
+        {
+            ret.emplace_back(el);
+        }
+        return ret;
+    }
+
+    template <typename TypeT>
+    std::vector<TypeT>&
+    append(std::vector<TypeT>& v1, const std::vector<TypeT>& v2)
+    {
+        for (const auto& el : v2)
+        {
+            v1.emplace_back(el);
+        }
+        return v1;
+    }
+
+    template <typename TypeT>
+    std::vector<TypeT>
+    subvector(const std::vector<TypeT>& v, int startPos, int endPos)
+    {
+        auto first = v.begin() + startPos;
+        auto last = v.begin() + endPos + 1;
+        std::vector<TypeT> vector(first, last);
+        return vector;
+    }
+
+    template <typename TypeT>
+    std::vector<TypeT>
+    subvector(const std::vector<TypeT>& v, int startPos)
+    {
+        return subvector(v, startPos, v.size()-1);
+    }
+
+    template <typename TypeT>
+    bool
+    startsWith(const std::vector<TypeT>& input, const std::vector<TypeT>& search)
+    {
+        if (search.size() > input.size())
+        {
+            return false;
+        }
+
+        for (unsigned int i = 0; i < search.size(); ++i)
+        {
+            if (input[i] != search[i])
+            {
+                return false;
+            }
+        }
+    }
+
+    template <typename TypeT>
+    bool
+    endsWith(const std::vector<TypeT>& input, const std::vector<TypeT>& search)
+    {
+        if (search.size() > input.size())
+        {
+            return false;
+        }
+
+        for (unsigned int i = input.size() - search.size(); i < input.size(); ++i)
+        {
+            if (input[i] != search[i])
+            {
+                return false;
+            }
+        }
+    }
+
+    // Todo: replace, split, ...
+}
+
---
+++ SimoxUtility/tests/algorithm/vector.cpp
@@ -0,0 +1,51 @@
+/**
+* @package    SimoxUtility
+* @author     Fabian Peller
+* @copyright  2021 Fabian Peller
+*/
+
+#define BOOST_TEST_MODULE SimoxUtility/algorithm/vector
+
+#include <random>
+
+#include <boost/test/included/unit_test.hpp>
+
+#include <SimoxUtility/algorithm/vector.hpp>
+
+
+struct SimoxVectorTestFixture
+{
+    const std::vector<int> vec { 1, 2, 3 };
+    const std::vector<int> vec2 { 4, 5, 6 };
+};
+
+
+BOOST_FIXTURE_TEST_SUITE(SimoxVectorTest, SimoxVectorTestFixture)
+
+
+BOOST_AUTO_TEST_CASE(test_appended)
+{
+    const std::vector<int> ref { 1, 2, 3, 4, 5, 6 };
+    auto res = simox::alg::appended(vec, vec2);
+
+    BOOST_CHECK_EQUAL(res.size(), ref.size());
+    for (unsigned int i = 0; i < res.size(); ++i)
+    {
+        BOOST_CHECK_EQUAL(res[i], ref[i]);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(test_subvec)
+{
+    const std::vector<int> ref { 3 };
+    auto res = simox::alg::subvector(vec, 3);
+
+    BOOST_CHECK_EQUAL(res.size(), ref.size());
+    for (unsigned int i = 0; i < res.size(); ++i)
+    {
+        BOOST_CHECK_EQUAL(res[i], ref[i]);
+    }
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
