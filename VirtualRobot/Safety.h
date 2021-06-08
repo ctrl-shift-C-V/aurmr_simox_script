3c1d073aa08facd745ec69edfa6ba33464f06533
Fabian Reister <fabian.reister@kit.edu>
2021-06-08 13:52:42 +0200

    fix

--- VirtualRobot/Safety.h
+++ VirtualRobot/Safety.h
@@ -1,4 +1,10 @@
 
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <Eigen/src/Core/Matrix.h>
+
+#include "MathTools.h"
+#include "VirtualRobot.h"
 #include "VirtualRobot/Robot.h"
 
 namespace VirtualRobot
@@ -10,5 +16,30 @@ namespace VirtualRobot
         float radius;
     };
 
-    Circle projectedBoundingCircle(const Robot& robot);
+    inline Circle projectedBoundingCircle(const Robot& robot)
+    {
+        // MathTools::createConvexHull2D(const std::vector<Eigen::Vector2f> &points)
+        std::vector<Eigen::Vector2f> nodePositions;
+
+        const auto nodes = robot.getRobotNodes();
+
+        std::transform(nodes.begin(),
+                       nodes.end(),
+                       std::back_inserter(nodePositions),
+                       [](const RobotNodePtr& node)
+                       { return node->getPoseInRootFrame(); });
+
+        const MathTools::ConvexHull2DPtr hull =
+            MathTools::createConvexHull2D(nodePositions);
+
+        std::vector<float> distances;
+
+        std::transform(hull->vertices.begin(),
+                       hull->vertices.end(),
+                       std::back_inserter(distances),
+                       [](const Eigen::Vector2f& pos) { return pos.norm(); });
+
+        const float maxDistance = *std::max(distances.begin(), distances.end());
+        return Circle{.center = Eigen::Vector2f::Zero(), .radius = maxDistance};
+    }
 } // namespace VirtualRobot
