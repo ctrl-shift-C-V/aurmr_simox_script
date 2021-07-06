fe10d86e09d9d9723355375473ae20ba9ea49588
Fabian Reister <fabian.reister@kit.edu>
2021-07-06 09:12:49 +0200

    license text

--- VirtualRobot/IK/platform/MecanumPlatformKinematics.cpp
+++ VirtualRobot/IK/platform/MecanumPlatformKinematics.cpp
@@ -1,3 +1,21 @@
+/**
+* This file is part of Simox.
+*
+* Simox is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2 of
+* the License, or (at your option) any later version.
+*
+* Simox is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* @author     Fabian Reister (fabian dot reister at kit dot edu)
+* @copyright  http://www.gnu.org/licenses/gpl-2.0.txt
+*             GNU General Public License
+*/
+
 
 #include "MecanumPlatformKinematics.h"
 
