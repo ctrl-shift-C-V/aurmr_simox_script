e2f8e752a46a528c4794293a2c951a38edd9aa82
Rainer Kartmann <rainer.kartmann@kit.edu>
2021-08-10 07:18:27 +0200

    Move core implementation to cpp

--- SimoxUtility/meta/type_name.h
+++ SimoxUtility/meta/type_name.h
@@ -3,17 +3,12 @@
 #include <string>
 #include <typeinfo>
 
-#include <boost/core/demangle.hpp>
-
 
 namespace simox::meta
 {
 
     /// Return the demangled type name in `typeInfo`.
-    inline std::string get_type_name(const std::type_info& typeInfo)
-    {
-        return boost::core::demangle(typeInfo.name());
-    }
+    std::string get_type_name(const std::type_info& typeInfo);
 
     /// Return the demangled (static) type name of `t`.
     template <typename T>
