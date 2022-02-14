37be14ea4c18b2c4ba464dc604255219a5ebab52
Rainer Kartmann <rainer.kartmann@kit.edu>
2022-02-14 10:29:18 +0100

    Use alias

--- SimoxUtility/json/util.h
+++ SimoxUtility/json/util.h
@@ -2,7 +2,7 @@
 
 #include <optional>
 
-#include "json.hpp"
+#include "json.h"
 
 
 namespace simox::json
@@ -10,16 +10,18 @@ namespace simox::json
 
     /**
      * @brief Get the value at the first key in `keys` contained in `j`.
+     *
      * @param j The JSON object.
      * @param keys The keys.
      * @return The value at the first contained key.
      *
      * @throw std::out_of_range If none of the keys is found in `j`.
      */
-    nlohmann::json::const_reference at_any_key(const nlohmann::json& j, const std::vector<std::string>& keys);
+    json::const_reference at_any_key(const json& j, const std::vector<std::string>& keys);
+
 
     template <class T>
-    T get_at_any_key(const nlohmann::json& j, const std::vector<std::string>& keys)
+    T get_at_any_key(const json& j, const std::vector<std::string>& keys)
     {
         return at_any_key(j, keys).get<T>();
     }
