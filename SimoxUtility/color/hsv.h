372eb3a3ce4a04644c0f59da348ec1570cca3104
Rainer Kartmann <rainer.kartmann@kit.edu>
2022-05-23 15:44:15 +0200

    Remove wrong and ill-defined overloads for rgb2hsv for ints. Improve doc for float

--- SimoxUtility/color/hsv.h
+++ SimoxUtility/color/hsv.h
@@ -8,14 +8,27 @@
 namespace simox::color
 {
 
-    /// Convert an RGB color to HSV (hue in degrees).
+    /**
+     * @brief Convert an RGB color to HSV.
+     *
+     * Hue is measured in degrees, i.e. [0, 360].
+     * All other channels (saturation, value, red, green, blue)
+     * are in the interval [0, 1].
+     *
+     * @param rgb The RGB color with all channels in range [0, 1].
+     * @return The HSV color in [0, 360] x [0, 1] x [0, 1].
+     */
     Eigen::Vector3f rgb_to_hsv(const Eigen::Vector3f& rgb);
-    /// Convert an HSV color to RGB (hue in degrees).
-    Eigen::Vector3f hsv_to_rgb(const Eigen::Vector3f& hsv);
 
-    /// Convert an RGB color to HSV (hue in degrees).
-    Eigen::Vector3i rgb_to_hsv(const Eigen::Vector3i& rgb);
-    /// Convert an HSV color to RGB (hue in degrees).
-    Eigen::Vector3i hsv_to_rgb(const Eigen::Vector3i& hsv);
+
+    /**
+     * @brief Convert an HSV color to RGB
+     *
+     * @see `simox::color::rgb_to_hsv()` for the expected intervals.
+     *
+     * @param hsv The HSV color in [0, 360] x [0, 1] x [0, 1]
+     * @return The RGB color with all channels in [0, 1].
+     */
+    Eigen::Vector3f hsv_to_rgb(const Eigen::Vector3f& hsv);
 
 }
