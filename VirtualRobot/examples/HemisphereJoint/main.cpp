8932a2117b0e6d9366ca1134718145c17c039efb
Rainer Kartmann <rainer.kartmann@kit.edu>
2022-06-03 19:04:31 +0200

    Update expressions and c++ example

--- VirtualRobot/examples/HemisphereJoint/main.cpp
+++ VirtualRobot/examples/HemisphereJoint/main.cpp
@@ -1,8 +1,11 @@
 #include "Expressions.h"
 
+#include <chrono>
 #include <iostream>
 
 #include <SimoxUtility/math/convert/deg_to_rad.h>
+#include <SimoxUtility/math/rescale.h>
+#include <SimoxUtility/math/statistics/measures.h>
 
 #include <VirtualRobot/RuntimeEnvironment.h>
 
@@ -17,10 +20,8 @@ int main(int argc, char* argv[])
 {
     RuntimeEnvironment::setCaption("Convert .iv to .wrl files");
 
-#if 0
-    RuntimeEnvironment::considerKey(
-                "input", ".iv file containing the (textured) mesh");
-#endif
+    RuntimeEnvironment::considerFlag(
+                "verbose", "Enable verbose output.");
 
     RuntimeEnvironment::processCommandLine(argc, argv);
 
@@ -33,18 +34,66 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    Expressions expr;
-    double lever = 1, theta0 = simox::math::deg_to_rad(25.);
-    double a1 = 0.0, a2 = 0.0;
-    expr.compute(a1, a2, lever, theta0);
+    const bool verbose = RuntimeEnvironment::hasFlag("verbose");
 
-    Eigen::Vector3d pos {expr.ex, expr.ey, expr.ez};
-
-
+    const double lever = 1, theta0 = simox::math::deg_to_rad(25.);
     std::cout << "(lever, theta0) = (" << lever << ", " << theta0 << ") " << std::endl;
-    std::cout << "(a1, a2) = (" << a1 << ", " << a2 << ") " << std::endl;
 
-    std::cout << "pos = (" << pos.transpose() << ")" << std::endl;
+    std::vector<double> a1s, a2s;
+    int num = 100;
+    double aMin = -0.7, aMax=0.7;
+    for (int i = 0; i < num; ++i)
+    {
+        double value = simox::math::rescale(double(i), double(0), double(num), aMin, aMax);
+        a1s.push_back(value);
+        a2s.push_back(value);
+    }
+
+
+    using time_point = std::chrono::system_clock::time_point;
+    using duration = std::chrono::nanoseconds;
+    const std::string unit = " ns";
+
+    std::vector<double> durations;
+    durations.reserve(a1s.size() * a2s.size());
+
+    for (double a1 : a1s)
+    {
+        for (double a2 : a2s)
+        {
+            const time_point start = std::chrono::system_clock::now();
+
+            Expressions expr;
+            expr.compute(a1, a2, lever, theta0);
+
+            Eigen::Vector3d pos {expr.ex, expr.ey, expr.ez};
+
+            // r_wrist_to_base = np.array([[exx, eyx, ezx], [exy, eyy, ezy], [exz, eyz, ezz]])
+            Eigen::Matrix3d ori;
+            ori << expr.exx, expr.eyx, expr.ezx,
+                   expr.exy, expr.eyy, expr.ezy,
+                   expr.exz, expr.eyz, expr.ezz;
+
+            const time_point end = std::chrono::system_clock::now();
+            durations.push_back(std::chrono::duration_cast<duration>(end - start).count());
+
+            if (verbose)
+            {
+                std::cout << "(a1, a2) = (" << a1 << ", " << a2 << ") \t ->  "
+                          << "pos = (" << pos.transpose() << ")" << std::endl;
+            }
+        }
+    }
+
+    double mean = simox::math::mean(durations);
+    double stddev = simox::math::stddev(durations, mean);
+    double min = simox::math::min(durations);
+    double max = simox::math::max(durations);
+
+    std::cout << "Durations:"
+              << " " << mean << " +- " << stddev << unit
+              << ", range: [" << min << unit << " to " << max << unit <<"]"
+              << std::endl;
 
     return 0;
 }
